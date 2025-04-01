d4521c6b89201e3e7dac9b92504d9208c10e00cc
Jonas Swiatek <jonas.swiatek@gmail.com>
2025-04-02 01:01:14 +0200

    Added loading state for create event button

--- src/src/Components/Events/CreateEvent.tsx
+++ src/src/Components/Events/CreateEvent.tsx
@@ -8,7 +8,7 @@ import { useQuery } from 'react-query';
 
 
 export function CreateEvent() : ReactElement {
-    const { register, handleSubmit, setError, clearErrors, formState: { errors } } = useForm<Inputs>();
+    const { register, handleSubmit, setError, clearErrors, formState: { errors, isSubmitting } } = useForm<Inputs>();
     const navigate = useNavigate();
 
     const { data: formats, isLoading: formatsLoading, error: formatsError } = useQuery<FormatResponse>(
@@ -88,7 +88,18 @@ export function CreateEvent() : ReactElement {
                                 </div>
                                 
                                 <div className="d-grid mt-4">
-                                    <button type='submit' className='btn btn-dark btn-lg'>Create Event</button>
+                                    <button 
+                                        type='submit' 
+                                        className='btn btn-dark btn-lg'
+                                        disabled={isSubmitting}
+                                    >
+                                        {isSubmitting ? (
+                                            <>
+                                                <span className="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>
+                                                Creating...
+                                            </>
+                                        ) : 'Create Event'}
+                                    </button>
                                 </div>
                             </form>
                         </div>
